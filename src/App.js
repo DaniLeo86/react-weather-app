0fca9bcdf64eb61ef8f6af90354fb00d590b1e40
DaniLeo86 <81173570+DaniLeo86@users.noreply.github.com>
2021-08-30 15:11:46 -0400

    added bootstrap and some html

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Danielle Leonard and is{" "}
-        <a
-          href="https://github.com/DaniLeo86/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by Danielle Leonard and is{" "}
+          <a
+            href="https://github.com/DaniLeo86/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Cloudy"
+          />
+          6°C
+        </div>
+        <div className="col-6"></div>
+      </div>
+    </div>
+  );
+}
