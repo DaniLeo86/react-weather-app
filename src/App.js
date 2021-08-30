f835d9b6be37dadaefd585018e0241c2a94e91c7
DaniLeo86 <81173570+DaniLeo86@users.noreply.github.com>
2021-08-30 09:48:54 -0400

    Added GitHub source link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Danielle Leonard and is{" "}
+        <a
+          href="https://github.com/DaniLeo86/react-weather-app"
+          target="_blank"
+        >
+          open sourced.
+        </a>
+      </footer>
+    </div>
+  );
 }
