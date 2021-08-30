c0386604ddbcdd83526b8ccd9f7f06a18d5664c1
DaniLeo86 <81173570+DaniLeo86@users.noreply.github.com>
2021-08-30 14:15:04 -0400

    added  rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default function App() {
         <a
           href="https://github.com/DaniLeo86/react-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           open sourced.
         </a>
